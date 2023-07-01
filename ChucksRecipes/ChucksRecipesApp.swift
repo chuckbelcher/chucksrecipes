1befc42d8dbaba24b95553ae5851330d5f27ad0d
Chuck Belcher <cfbelcher@pateogroup.com>
2023-07-01 19:57:46 -0400

    Initial Commit

---
+++ ChucksRecipes/ChucksRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChucksRecipesApp.swift
+//  ChucksRecipes
+//
+//  Created by Chuck Belcher on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ChucksRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
