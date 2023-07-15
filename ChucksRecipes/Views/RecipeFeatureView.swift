7bb5076cd7c9f750a4295ec562607c2f07f68c02
Chuck Belcher <cfbelcher@pateogroup.com>
2023-07-15 19:23:22 -0400

    Added a tab view with a geometry reader. Displaying a rectangle for each recipe with a featured status of true.

--- ChucksRecipes/Views/RecipeFeatureView.swift
+++ ChucksRecipes/Views/RecipeFeatureView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct RecipeFeatureView: View {
     @EnvironmentObject var recipeModel: RecipeModel
+    
     var body: some View {
-        Text("Hello Recipe Feature View")
+        GeometryReader { geo in
+            TabView {
+                ForEach (0..<recipeModel.recipies.count) { index in
+                    
+                    if recipeModel.recipies[index].featured {
+                        Rectangle()
+                            .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
+                    }
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+            .indexViewStyle((PageIndexViewStyle(backgroundDisplayMode: .always)))
+        }
     }
 }
 
 struct RecipeFeatureView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeFeatureView()
+            .environmentObject(RecipeModel())
     }
 }
